e203cbd53330ce2de4b3642766142bbc9385c186
ricard-gi <ricard.hernandez.gi@gmail.com>
2024-02-09 18:45:24 +0100

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    < div className="p-20">
+      <h1 className="text-2xl text-red-500">Hola React, hola Tailwind...</h1>
+       
+    </div>
+  )
+}
+
+export default App
